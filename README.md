28ccfdece3fb5d71eadf248d364a0827c2c3878a
Taek Joo Kim <taicki@gmail.com>
2011-12-30 16:40:56 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Timer
+
+A Chrome extension to set timers from the omnibox. It is good for Keyboard
+lovers. Type 'tm' and space, then you can set the timer from the
+omnibox.
+
+## Install
+[chrome web store][4]
+
+## Examples
+* `10 Lunch` : the default unit is minute. This timer will ring in 10 minutes
+* `30s` : 30 seconds
+* `20m Go home` : 20 minutes
+* `1h meeting` : 1 hour
+
+## Media files
+I used the icons from [this page][2] introduced in [this
+article][1]. The alarm bell is downloaded from [this page][3].
+
+[1]: http://www.smashingmagazine.com/2009/02/16/50-beautiful-useful-and-free-icon-sets/
+[2]: http://sabramedia.com/icons
+[3]: http://www.pachd.com/sounds.html
---
+++ src/background.js
@@ -0,0 +1,67 @@
+var nid = 0;
+
+function parseTime(str) {
+  var num = parseInt(str);
+  if (isNaN(num))
+    return null;
+  var last = str.charAt(str.length - 1);
+  var mul;
+  switch (last) {
+  case 'h': mul = 60 * 60; break;
+  case 'm': mul = 60; break;
+  case 's': mul = 1; break;
+  default: mul = 60; break;
+  }
+  return num * mul;
+}
+
+function setNotification(text) {
+  var arr = text.split(/\s+/);
+  var seconds = parseTime(arr.shift());
+  if (!seconds) {
+    console.log("parse error: " + text);
+    return;
+  }
+
+  nid += 1;
+  var id = nid;
+
+  var ms = seconds * 1000;
+  var title = 'Timer done!';
+  var desc = 'Timer done!';
+  if (arr.length > 0)
+    desc = arr.join(" ");
+
+  console.log(id + ": setup " + seconds + " seconds from "
+              + new Date().toString());
+
+  if (window.webkitNotifications) {
+    var notification = window.webkitNotifications.createNotification(
+      "48.png",
+      title,
+      desc
+    );
+    setTimeout(function() {
+      notification.show();
+      var alarm = document.getElementById('alarm');
+      if (alarm) {
+        alarm.play();
+      }
+      console.log(id + ": notified at " + new Date().toString());
+    }, ms);
+  }
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text){
+  setNotification(text);
+});
+
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'Timer set: &lt;time&gt; [&lt;message&gt;]'
+  });
+}
+
+chrome.omnibox.onInputStarted.addListener(function() {
+  resetDefaultSuggestion();
+});
